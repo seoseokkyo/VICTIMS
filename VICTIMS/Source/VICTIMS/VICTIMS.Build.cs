eac7fe594c8e4f3a071856f9503d98cd2cf041cf
seoseokkyo <seoseokkyo@naver.com>
2024-06-20 13:12:28 +0900

    Filter추가

--- VICTIMS/Source/VICTIMS/VICTIMS.Build.cs
+++ VICTIMS/Source/VICTIMS/VICTIMS.Build.cs
@@ -9,5 +9,17 @@ public class VICTIMS : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-	}
+
+        PublicIncludePaths.AddRange(new string[] {
+                "VICTIMS/Public",
+                "VICTIMS/Public/Player",
+                "VICTIMS/Public/System",
+            });
+
+        PrivateIncludePaths.AddRange(new string[] {
+                "VICTIMS/Private",
+                "VICTIMS/Private/Player",
+                "VICTIMS/Private/System",                
+            });
+    }
 }
